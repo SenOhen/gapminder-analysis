caf9e597e0b540b59a9426b7b7e09b169eb487e9
Senyo Ohene <senohene@gmail.com>
2023-02-06 18:42:52 -0500

    set up code files

---
+++ src/gapmindercode.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+
+url = "https://raw.githubusercontent.com/jennybc/gapminder/main/inst/extdata/gapminder.tsv"
+
+gapminderdf = pd.read_table(url)
+
+print(gapminderdf.head())
